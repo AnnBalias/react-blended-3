8637fd3c2c232f54a540364d45583a9219e3b72f
AnnBalias <annbalias@gmail.com>
2025-02-26 14:56:08 +0200

    country

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,12 +1,35 @@
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import Container from '../components/Container/Container';
-import Heading from '../components/Heading/Heading';
+import CountryInfo from '../components/CountryInfo/CountryInfo';
+import GoBackBtn from '../components/GoBackBtn/GoBackBtn';
 import Section from '../components/Section/Section';
+import { useEffect, useMemo, useState } from 'react';
+import { fetchCountry } from '../service/countryApi';
+import Loader from '../components/Loader/Loader';
+
 
 const Country = () => {
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  const goBackUrl = useMemo(() => location.state?.from || '/', [location.state]);
+
+  const {countryId} = useParams();
+  const [countryData, setCountryData] = useState(null)
+
+  useEffect(() => {
+    const getCountry = async () => {
+      const getData = await fetchCountry(countryId); 
+      setCountryData(getData);
+    };
+    getCountry();
+  }, [countryId]);
+  
   return (
     <Section>
       <Container>
-        <Heading title="SearchCountry" bottom />
+        <GoBackBtn goBackUrl={goBackUrl} />
+        {!countryData ? <Loader /> : <CountryInfo countryData={countryData} />}
       </Container>
     </Section>
   );
